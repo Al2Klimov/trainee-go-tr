d21df43378b8110f93d592389aa5783385e27dba
Justin Mueller <justin.mueller@icinga.com>
2020-06-19 16:01:28 +0200

    implement tr in go
    refs #9

--- main.go
+++ main.go
@@ -9,33 +9,31 @@ import (
 )
 
 func main() {
-	var correctedData [][]byte
+	const chunkSize = 4096
 	cArgs := os.Args[1:]
 	buf := bufio.NewReader(os.Stdin)
+	buffer := make([]byte, chunkSize)
 
 	for {
-		data, dataErr := buf.ReadBytes('\n')
+		_, dataErr := buf.Read(buffer)
 		if dataErr != nil && dataErr != io.EOF {
 			fmt.Fprintln(os.Stderr, dataErr)
 			os.Exit(1)
 		}
 
-		dataSplit := bytes.Split(data, []byte(""))
-
-		for i, j := range dataSplit {
-			if bytes.Compare(j,[]byte(cArgs[0])) == 0 {
-				dataSplit[i] = []byte(cArgs[1])
-			}
-		}
-
-		correctedData = append(correctedData, bytes.Join(dataSplit,[]byte("")))
-
 		if dataErr == io.EOF {
 			break
 		}
-	}
 
-	for i := range correctedData {
-		fmt.Printf("%s", correctedData[i])
+		data := bytes.Split(bytes.Trim(buffer, string(0x0)), []byte("\n"))
+
+		for i := range data {
+			for j := range data[i] {
+				if string(data[i][j]) == cArgs[0] {
+					data[i][j] = []byte(cArgs[1])[0]
+				}
+			}
+		}
+		fmt.Printf("%s", bytes.Join(data, []byte("\n")))
 	}
 }
